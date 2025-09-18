a828fa3e62e8eb9b0176c85dd83b76d074f5b981
ThatCrazyTechMan <kabir@marathe.net>
2025-09-18 10:05:19 +0100

    Work done for 18th September

---
+++ fileAppend.py
@@ -0,0 +1,20 @@
+## Read text file
+try:
+    with open("text_file.txt","r") as text_file:
+        print(text_file.read())
+except FileNotFoundError:
+    print("File not found")
+
+try:
+    with open("text_file.txt","a") as text_file:
+        text_file.write("Hello World\n")
+
+except FileNotFoundError:
+    print("File not found")
+
+# Read specific lines
+try:
+    with open("text_file.txt","r") as text_file:
+        file = text_file.readlines()
+except FileNotFoundError:
+    print("File not found")
