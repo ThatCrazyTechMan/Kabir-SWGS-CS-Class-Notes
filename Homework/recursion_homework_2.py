15acd28f798830e53b24345846cedcdfa6f43fb5
ThatCrazyTechMan <kabir@marathe.net>
2025-09-23 10:31:46 +0100

    Done recursive functions 2, still buggy AF

--- Homework/recursion_homework_2.py
+++ Homework/recursion_homework_2.py
@@ -2,16 +2,14 @@ input_arr = []
 user_input = (input("Enter a series of numbers: "))
 numbers = user_input.split()
 for number in numbers:
-    input_arr.append(int(number))
+    input_arr.append(float(number))
 print(f"You entered {input_arr}")
 
-
-
 def add_numbers(adding_input):
-    if adding_input == 0:
+    if adding_input == 1:
         return 0
     else:
-        return adding_input[0] + add_numbers(adding_input[1:])
+        return adding_input [0] + add_numbers(adding)
 
 result = add_numbers(input_arr)
 print(result)
