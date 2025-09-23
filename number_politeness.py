f9c53f45469a48410bd37573d09225db1e753505
ThatCrazyTechMan <kabir@marathe.net>
2025-09-23 12:26:04 +0100

    Buggy af politeness checker

---
+++ number_politeness.py
@@ -0,0 +1,26 @@
+import math as math
+import random
+
+input_number = int(input("Enter a number: "))
+arr = []
+
+
+for i in range(1, input_number + 1):
+    arr.append(i)
+
+print(arr)
+
+
+def isConsecutive(num1, num2):
+    if num2 - num1 == 1:
+        return True
+    else:
+        return False
+
+
+
+def politeness(input_arr):
+    current_sum = 0
+    while current_sum != input_number:
+        for in arr:
+
