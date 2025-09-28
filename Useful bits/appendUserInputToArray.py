641655496e64e0cad8fe927f7171c81a137b6a1a
ThatCrazyTechMan <kabir@marathe.net>
2025-09-28 11:40:39 +0100

    Added "Useful bits" folder to store reusable, useful pieces of code

---
+++ Useful bits/appendUserInputToArray.py
@@ -0,0 +1,7 @@
+
+input_arr = []
+user_input = (input("Enter a series of numbers: "))
+for entry in user_input:
+    input_arr.append(int(entry))
+# Change the "int()" to a "str()" if you want to append text
+print(f"You entered {input_arr}")
