8859bd4efa16405a4b38f7ab1312cf02949a0ce7
ThatCrazyTechMan <kabir@marathe.net>
2025-10-07 12:16:34 +0100

    Finished pangramChecker

---
+++ pangramChecker.py
@@ -0,0 +1,24 @@
+import string
+
+input_arr = []
+user_input = (input("Enter a sentence: "))
+for entry in user_input:
+    input_arr.append(str(entry))
+# Change the "int()" to a "str()" if you want to append text
+print(f"You entered {input_arr}")
+
+ALPHABET = []
+for i in string.ascii_lowercase:
+    ALPHABET.append(i)
+
+foundLetters = []
+for i in ALPHABET:
+    if i in user_input:
+        foundLetters.append(i)
+
+differenceArray = list(set(ALPHABET) - set(foundLetters))
+
+if len(differenceArray)==0:
+    print("This is a valid pangram")
+else:
+    print(f"This is an invalid pangram. You missed {differenceArray}")
