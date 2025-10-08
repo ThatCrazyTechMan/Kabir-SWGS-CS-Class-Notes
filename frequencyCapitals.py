940e3e2c5465cff2537168f2d0a5200b4cf2c2db
ThatCrazyTechMan <kabir@marathe.net>
2025-10-08 11:33:29 +0100

    Started frequencyCapitals.py and added an OOP folder

---
+++ frequencyCapitals.py
@@ -0,0 +1,24 @@
+import string
+
+input_arr = []
+user_input = (input("Enter a word. Ensure that you only use letters A-F: "))
+for entry in user_input:
+    input_arr.append(str(entry))
+# Change the "int()" to a "str()" if you want to append text
+print(f"You entered {input_arr}")
+
+validLetters = ['a, b, c, d, e, f']
+
+for i in input_arr:
+    if i in validLetters == False:
+        print(f"Invalid letter: {i}")
+        break
+
+
+def most_common(lst):
+    return max(set(lst), key=lst.count)
+
+mostCommonLetter = most_common(input_arr)
+
+for i in input_arr:
+    if i == mostCommonLetter:
