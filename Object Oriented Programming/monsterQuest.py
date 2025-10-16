811fe513ff678d0e68e1786b675330ba56a4cf6c
ThatCrazyTechMan <kabir@marathe.net>
2025-10-16 09:32:51 +0100

    Updated monsterquest a lot

--- Object Oriented Programming/monsterQuest.py
+++ Object Oriented Programming/monsterQuest.py
@@ -1,9 +1,43 @@
+import random
+
+class Game:
+    def __init__(self, player_health):
+        self.monsters = []
+        friend1 = Friend("Sonny", "Wardrdrdr I do???", "67", "Put 'er there pal", "Monster energy")
+        self.monsters.append(friend1)
+        enemy1 = Enemy("Lilly", "I'm not posh, I'm comfortable!!!", "41", "I'm going to crashout!!", "Here's a knuckle sandwich!!")
+        self.monsters.append(enemy1)
+        self.player_health = player_health
+        print("Completed method")
+        self.play_game()
+
+    def get_monsters_list(self):
+        return self.monsters
+    def take_damage(self, damage):
+        self.player_health -= damage
+    def get_health(self):#
+        return self.player_health
+
+
+    def play_game(self):
+        opponent = random.choice(self.monsters)
+        choice = input("Will you (f)ight or (h)ighfive?")
+        if choice == "f":
+            result = opponent.fight()
+            self.player_health += result
+        elif choice == "h":
+            result = opponent.highFive
+            self.player_health += result
+
+
+
+        
+
 class Monster:
     def __init__(self, name, catchphrase, health):
         self.name = name
         self.catchphrase = catchphrase
         self.health = int(health)
-
 
         # Call me David the way I Getter
     def get_name(self):
@@ -18,49 +52,50 @@ class Monster:
         print(self.catchphrase)
 
         # Setters
-    def take_damage(self, damage_amount):
+    def hurt(self, damage_amount):
         self.health = self.health - damage_amount
     def change_catchphrase(self, new_catchphrase):
         self.catchphrase = new_catchphrase
+        print('You did 10 points of damage!!')
+    def changeGift(self, new_gift):
+        self.gift = new_gift
 
-class Weapon:
-    def __init__(self, damage_amount, noise, target):
-        self.damage_amount = damage_amount
-        self.noise = noise
-        self.target = target
+class Friend(Monster):
+    def __init__(self, name, catchphrase, health, highfive_dialogue, gift):
+        super().__init__(name, catchphrase, health)
+        self.highfive_dialogue = highfive_dialogue
+        self.gift = gift
+        self.isFriend = True
 
-    # Getters
-    def get_noise(self):
-        return self.noise
-    def get_target(self):
-        return self.target
+    def highFive(self):
+        print(self.name + ' raises their hand and says... ' + self.highfive_dialogue)
+        print(self.name + ' then gives you ' + self.gift)
+        return 10
+    def giveGift(self):
+        print(self.name + ' gives you ' + self.gift)
+    def fight(self):
+        print("Hey! You can't fight me! I'm your friend!!")
+        return -10
 
-    # Setters
-    def attack(self):
 
+class Enemy(Monster):
+    def __init__(self, name, catchphrase, health, highfive_dialogue, gift):
+        super(). __init__(name, catchphrase, health,)
+        self.highfive_dialogue = highfive_dialogue
+        self.gift = gift
+        self.isFriend = False
 
+    def highFive(self):
+        print(self.highfive_dialogue)
+        return -10
+    def giveGift(self):
+        print(self.gift)
+    def fight(self):
+        print("AAARRHGGGHH!!! I'll get you next time!!")
+        self.health -= 10
+        return 10
 
-damage_amount = 10
+game1 = Game(100)
 
-Puran = Monster('Puran', 'He was molested as a baby boy', '67')
-print(Puran.get_name())
-print(Puran.get_catchphrase())
-print(Puran.get_health())
-Puran.take_damage(damage_amount)
-print(Puran.get_health())
-
-Sonny = Monster('Sonny', 'Wadrdrdrdd I do?', '50')
-print(Sonny.get_name())
-print(Sonny.get_catchphrase())
-print(Sonny.get_health())
-Sonny.take_damage(damage_amount)
-print(Sonny.get_health())
-
-Vince = Monster('Vince', "My collar's blue, but my neck is red", '68')
-print(Vince.get_name())
-Vince.speak()
-print(Vince.get_health())
-Vince.take_damage(damage_amount)
-print(Vince.get_health())
 
 
