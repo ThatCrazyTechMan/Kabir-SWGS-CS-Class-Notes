7a886c15717d6fb667a2c40d89ae18b98a5d8583
ThatCrazyTechMan <kabir@marathe.net>
2025-12-03 10:27:56 +0000

    edited linkedList.py

--- Object Oriented Programming/Data Structures/linkedList.py
+++ Object Oriented Programming/Data Structures/linkedList.py
@@ -28,6 +28,12 @@ class LinkedList:
             current_node = current_node.getNext()
         return current_node
 
+    def printAll(self):
+        current_node = self.getHead()
+        while current_node.getNext() is not None:
+            print(current_node.getData())
+            current_node = current_node.getNext()
+
     def insert_at_begin(self, new_data):
         new_node = Node(new_data)
         if self.getHead is None:
@@ -38,11 +44,25 @@ class LinkedList:
             self.setHead(new_node)
 
     def insert_at_end(self, new_data):
-        new_node = Node(new_data)
-        last_node = self.getLast()
-        last_node.setNext(new_node)
-        last_node = self.getLast()
-        last_node.setNext(None)
+        if self.getHead() is None:
+            node = Node(new_data)
+            self.setHead(node)
+        else:
+            new_node = Node(new_data)
+            last_node = self.getLast()
+            last_node.setNext(new_node)
+
+    def insert_at_index(self, new_data, index):
+        counter = 0
+        current_node = self.getHead()
+        if index == 0:
+            node = Node(new_data)
+            self.setHead(node)
+        while counter != index -1:
+            current_node = current_node.getNext()
+            counter += 1
+        current_node.setNext(new_data)
+
 
     def delete_at_begin(self):
         current_node = self.getHead()
@@ -53,6 +73,18 @@ class LinkedList:
             self.setHead(next_node)
             current_node = None
 
+    def delete_at_end(self):
+        current_node = self.getLast()
+        current_node.setData(None)
 
 
 
+list = LinkedList()
+list.insert_at_begin('France')
+list.insert_at_begin('Germany')
+list.insert_at_begin('Spain')
+list.insert_at_begin('Belgium')
+list.insert_at_end('Brazil')
+list.delete_at_begin()
+list.insert_at_index('Norway', 2)
+list.printAll()
