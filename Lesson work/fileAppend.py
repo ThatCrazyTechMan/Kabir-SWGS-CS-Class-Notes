2cbba52bf6af0ae77a5d91a73b7d691f56a173b1
ThatCrazyTechMan <kabir@marathe.net>
2026-02-06 10:36:52 +0000

    Refactored things, and started the skeleton program

--- Lesson work/fileAppend.py
+++ Lesson work/fileAppend.py
@@ -1,12 +1,12 @@
 ## Read text file
 try:
-    with open("text_file.txt","r") as text_file:
+    with open("text_file.txt", "r") as text_file:
         print(text_file.read())
 except FileNotFoundError:
     print("File not found")
 
 try:
-    with open("text_file.txt","a") as text_file:
+    with open("text_file.txt", "a") as text_file:
         text_file.write("Hello World\n")
 
 except FileNotFoundError:
@@ -14,7 +14,7 @@ except FileNotFoundError:
 
 # Read specific lines
 try:
-    with open("text_file.txt","r") as text_file:
+    with open("text_file.txt", "r") as text_file:
         file = text_file.readlines()
 except FileNotFoundError:
     print("File not found")
--- Lesson work/fileHandling.py
+++ Lesson work/fileHandling.py
@@ -1,12 +1,12 @@
 #Method 1
-textFile = open("textFile.txt","w")
+textFile = open("textFile.txt", "w")
 textFile.write("Hello World\n")
 textFile.close()
 
 
 #Method 2
 try:
-    with open("text_file.txt","w") as text_file:
+    with open("text_file.txt", "w") as text_file:
         text_file.write("Hello World\n")
 except FileNotFoundError:
         print("File not found")
